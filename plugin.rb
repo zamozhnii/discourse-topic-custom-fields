2a6871700ef7c1ea87d1f516ade4c0310d86209b
ihor <zamozhnii@gmail.com>
2025-08-12 14:13:39 +0200

    fix

--- plugin.rb
+++ plugin.rb
@@ -7,17 +7,10 @@
 enabled_site_setting :enable_discourse_category_locale
 
 after_initialize do
-  # Безопасные проверки, чтобы не словить nil
-  add_to_serializer(:basic_category, :local_lang, false) do
-    object.respond_to?(:custom_fields) && object.custom_fields ? object.custom_fields["local_lang"] : nil
-  end
-  add_to_serializer(:basic_category, :hreflang_code, false) do
-    object.respond_to?(:custom_fields) && object.custom_fields ? object.custom_fields["hreflang_code"] : nil
-  end
   add_to_serializer(:category, :local_lang, false) do
-    object.respond_to?(:custom_fields) && object.custom_fields ? object.custom_fields["local_lang"] : nil
+    object.custom_fields && object.custom_fields["local_lang"]
   end
   add_to_serializer(:category, :hreflang_code, false) do
-    object.respond_to?(:custom_fields) && object.custom_fields ? object.custom_fields["hreflang_code"] : nil
+    object.custom_fields && object.custom_fields["hreflang_code"]
   end
 end
