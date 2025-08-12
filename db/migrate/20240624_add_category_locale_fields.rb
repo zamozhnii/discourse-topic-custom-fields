f7238d9c92cfb9c6a35c25db3bff25812a945f35
ihor <zamozhnii@gmail.com>
2025-08-12 12:35:25 +0200

    rem

--- db/migrate/20240624_add_category_locale_fields.rb
+++
@@ -1,9 +0,0 @@
-class AddCategoryLocaleFields < ActiveRecord::Migration[6.0]
-  def up
-    # Нет нужды добавлять структуру, custom_fields уже есть
-    # Миграция не нужна, поля появятся в custom_fields сами
-  end
-
-  def down
-  end
-end
